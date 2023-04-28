c183c23691d9c20cc039000da9d2b8fb0baef851
gus <gustavobelato1@gmail.com>
2023-04-28 12:34:01 -0300

    Initial commit

---
+++ functions.py
@@ -0,0 +1,28 @@
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt", "w") as file:
+        pass
+
+
+FILEPATH = 'todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == '__main__':
+    print("Hello from functions")
+    print(get_todos())
